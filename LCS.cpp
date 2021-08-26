9e9dc09be300ad2bed947628db62845a53c13fdf
UserWin <USER@DESKTOP-8M2NJ75>
2021-08-27 01:00:07 +0700

    Update0827 + Update Warning_C6262: data to heap

---
+++ LCS.cpp
@@ -0,0 +1,84 @@
+﻿// Cho hai xâu ký tự. Tìm độ dài xâu con chung nhỏ nhất giữa chúng. 
+//Ví dụ với 2 xâu “quetzalcoatl” và “tezcatlipoca” thì xâu con chung dài nhất sẽ là “ezaloa” với độ dài 6.
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int max(int, int);
+void input(string&, string&);
+void LongestCommonString(string, string, string&);
+
+int main()
+{
+    string A, B;
+    string save = "";
+    int res;
+    input(A, B);
+    LongestCommonString(A, B, save);
+    cout << "Size of Logest Common string is: " << save.size() << endl;
+    cout << "LCS is: " << save << endl;
+    
+    return 0;
+}
+
+void input(string& A, string& B) {
+    A = "adksbvmbyi gfd";
+    B = "dncjkfihtbnmn dlk";
+    A.erase(remove(A.begin(), A.end(), ' '), A.end());
+    B.erase(remove(B.begin(), B.end(), ' '), B.end());
+}
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+void LongestCommonString(string A, string B, string& save) {
+    int lA = A.size();
+    int lB = B.size();
+
+    //int temp[100][100];
+    int** temp = new int* [lA+1];
+    for (int i = 0; i < lA + 1; ++i)
+        temp[i] = new int[lB+1];
+
+    for (int i = 0; i <= lA; i++) {
+        for (int j = 0; j <= lB; j++)
+            if (i == 0 || j == 0)
+                temp[i][j] = 0;
+    }
+    int res = 0;
+    for (int i = 1; i <= lA; i++) {
+        for (int j = 1; j <= lB; j++) {
+            if (A[i-1] == B[j-1])
+                temp[i][j] = temp[i - 1][j - 1] + 1;
+            else {
+                temp[i][j] = max(temp[i - 1][j], temp[i][j - 1]);
+            }
+        }
+    }
+
+    /*for (int i = 0; i <= lA; i++) {
+        for (int j = 0; j <= lB; j++)
+            cout << temp[i][j] << "\t";
+        cout << endl;
+    }*/
+    res = temp[lA][lB];
+    int i = lA;
+    int j = lB;
+    while (res > 0) {
+        if (A[i-1] == B[j-1]) {
+            //save += A[i-1];
+            save = A[i - 1] + save;
+            i--;
+            j--;
+            res--;
+        }
+        else
+            if (temp[i][j-1] < temp[i-1][j])
+                i--;
+            else
+                j--;
+    }
+}
+
+
---
+++ SMK.cpp
@@ -0,0 +1,88 @@
+﻿// SMK.cpp : Cho một dãy A gồm n số nguyên và một số nguyên dương k. 
+//Hãy tìm một dãy con (không nhất thiết phải liên tiếp nhau) dài nhất có tổng các số chia hết cho số k.
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void input(vector<int>&, int&, int&);
+int max(int, int);
+void SubarrayModk(vector<int>, int, int, vector<int>&);
+
+
+int main()
+{
+    vector<int> arr, save;
+    int n, k;
+    input(arr, n, k);
+    SubarrayModk(arr, n, k, save);
+    cout << "Lenght of sub-array: " << save.size() << endl;
+    for (int i = 0; i < save.size(); i++) {
+        cout << save[i] << "\t";
+    }
+    cout << endl;
+    return 0;
+}
+
+void input(vector<int>& arr, int& n, int& k) {
+    arr = { 5,6,11,2,4,13,21,9,7 };
+    //arr = { 5,6,11,2,4,9,7 };
+    n = arr.size();
+    k = 11;
+}
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+void SubarrayModk(vector<int> arr, int n, int k, vector<int>& save) {
+    /*int temp[100][100];*/
+    int** temp = new int* [n];
+    for (int i = 0; i < n; ++i)
+        temp[i] = new int[k];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++)
+            if (arr[i] % k == j)
+                temp[i][j] = 1; 
+            else
+                temp[i][j] = 0;
+    }
+    //temp[][]: mảng lưu độ dài
+    for (int i = 1; i < n; i++) {
+        int r = arr[i] % k;
+        for (int j = 0; j < k; j++) {
+            if (temp[i - 1][(k - abs(j-r)) % k] > 0) 
+                temp[i][j] = max(temp[i - 1][j], temp[i - 1][(k - abs(j-r)) % k] + 1);
+            //
+            //ta có tính chất: a mod k = x; b mod k = y ==> (a+b) mod k = (x+y) mod k
+            else
+                temp[i][j] = max(temp[i - 1][j], temp[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++)
+            cout << temp[i][j] << "\t";
+        cout << endl;
+    }
+
+    int j = n - 1;
+    int flag = 0;
+    //Max-res = temp[n-1][flag] vì:
+    //  (+) n-1: xét duyệt toàn bộ arr, trường hợp tốt nhất là Max-res = arr.siize()
+    //  (+) flag=0: vì ta lấy kết quả chia hết, nên mod = 0.
+    while (j > 0) {
+        int r = arr[j] % k;
+        if ((temp[j - 1][(k -abs(flag - r)) % k]>0) && (temp[j][flag] > temp[j - 1][(k - abs(flag - r)) % k])) {
+            save.push_back(arr[j]);
+            j--;
+            flag = (flag - r + k) % k;
+        }
+        else {
+            j--;
+        }
+    }
+    if (temp[0][flag] > 0)
+        save.push_back(arr[0]);
+}
